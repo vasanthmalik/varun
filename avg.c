7d3c5412d05a36ff04328c6d314fb0f0cd074ba8
vasanthmalik <31505734+vasanthmalik@users.noreply.github.com>
2018-02-19 16:14:57 +0530

    Create avg.c

---
+++ avg.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int a[20],avg,sum=0,i,n;
+printf("enter the noOf elements you want:");
+scanf("%d",&n);
+printf("\n enter the numbers:");
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+sum+=a[i];
+}
+avg=sum/2;
+printf("\n the average is %d",avg);
+return 0;
+}
