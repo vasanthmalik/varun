6fd69cf432f4d8afa341be52e4fcf72fa04b8976
vasanthmalik <31505734+vasanthmalik@users.noreply.github.com>
2018-03-02 15:47:07 +0530

    Create mulseven.c

---
+++ mulseven.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int num;
+printf("enter the number to check:");
+scanf("%d",&num);
+if("num%7==0)
+{
+printf("\n yes it is multiple of 7");
+}
+else
+{
+printf("\n no it is not multiple of 7");
+}
+return 0;
+}
